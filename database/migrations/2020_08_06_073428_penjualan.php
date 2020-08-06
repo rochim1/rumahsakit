bbf3f36928153bfe4884d598f72a17058ec1edba
rochim1 <muhammad.0206@students.amikom.ac.id>
2020-08-06 19:24:43 +0700

    create table

---
+++ database/migrations/2020_08_06_073428_penjualan.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Penjualan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Schema::table('penjualan', function (Blueprint $table) {
+        Schema::create('penjualan', function (Blueprint $table) {
+            $table->id('id_penjualan');
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id_user')->on('users');
+            // ->onDelete('cascade') tidak ada karena agar user yang memiliki riwayat transaksi tidak bisa dihapus
+            //  $table->foreignId('user_id')->constrained('users'); juga bisa
+            $table->date('jadwal_pengiriman')->nullable();
+            $table->bigInteger('total_harga');
+            $table->bigInteger('bayar');
+            $table->bigInteger('kembalian');
+            $table->timestamps();
+            $table->softDeletes('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_08_06_121938_admin.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Admin extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->id('id_admin'); // sudah auto increment
+            $table->string('name')->comment('nama admin');
+            $table->string('alamat', 200);
+            $table->bigInteger('telpon');
+            $table->string('email')->unique();
+            $table->string('password')->default('sha1');
+            $table->timestamps();
+            $table->softDeletes('deleted_at');
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
